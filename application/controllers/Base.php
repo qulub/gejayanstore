9fcea5ddf5c6bde9d861d1c1aa378c417cd327ef
yussan <yusuf.hi@students.amikom.ac.id>
2015-05-29 00:31:31 +0700

    malam di jdv

--- application/controllers/Base.php
+++ application/controllers/Base.php
@@ -79,4 +79,45 @@ class Base extends CI_Controller
 		else if($sisa == 0){return '1';}
 		else{return $sisa;}
 	}
+	/*
+	* LAIN LAIN
+	*/
+	//send message
+	public function sendemail($destination,$subject,$body)
+	{
+		$url = 'https://api.sendgrid.com/';
+		$user = 'qulub';
+		$pass = 'GN-003gundam';
+		$params = array(
+		    'api_user'  => $user,
+		    'api_key'   => $pass,
+		    'to'        => $destination,
+		    'subject'   => $subject,
+		    'from'      => 'noreply@gejayanstore.com',
+		  );
+		  $params['html'] = '
+		  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><meta name="viewport" content="width=device-width"><title>My email message created with BeeFree</title><meta http-equiv="Content-Type" content="text/html; charset=utf-8">  <meta name="viewport" content="width=device-width, initial-scale=1.0">  <meta http-equiv="X-UA-Compatible" content="IE=edge">  <meta name="format-detection" content="telephone=no">  <style type="text/css">  /* RESET */  #outlook a {padding:0;} body {width:100% !important; -webkit-text-size-adjust:100%; -ms-text-size-adjust:100%; margin:0; padding:0; mso-line-height-rule:exactly;}  table td { border-collapse: collapse; }  .ExternalClass {width:100%;}  .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {line-height: 100%;}  table td {border-collapse: collapse;}  /* IMG */  img {outline:none; text-decoration:none; -ms-interpolation-mode: bicubic;}  a img {border:none;}  /* Becoming responsive */  @media only screen and (max-device-width: 480px) {  table[id="container_div"] {max-width: 480px !important;}  table[id="container_table"], table[class="image_container"], table[class="image-group-contenitor"] {width: 100% !important; min-width: 320px !important;}  table[class="image-group-contenitor"] td, table[class="mixed"] td, td[class="mix_image"], td[class="mix_text"], td[class="table-separator"], td[class="section_block"] {display: block !important;width:100% !important;}  table[class="image_container"] img, td[class="mix_image"] img, table[class="image-group-contenitor"] img {width: 100% !important;}  table[class="image_container"] img[class="natural-width"], td[class="mix_image"] img[class="natural-width"], table[class="image-group-contenitor"] img[class="natural-width"] {width: auto !important;}  a[class="button-link justify"] {display: block !important;width:auto !important;}  td[class="table-separator"] br {display: none;}  td[class="cloned_td"]  table[class="image_container"] {width: 100% !important; min-width: 0 !important;} } table[class="social_wrapp"] {width: auto;} </style></head>
+		  <body bgcolor="#eaeaea">'.$body.'</body>
+		  </html>
+		  ';
+		  $params['text'] = $params['html'];
+		$request =  $url.'api/mail.send.json';
+		// Generate curl request
+		$session = curl_init($request);
+		// Tell curl to use HTTP POST
+		curl_setopt ($session, CURLOPT_POST, true);
+		// Tell curl that this is the body of the POST
+		curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+		// Tell curl not to return headers, but do return the response
+		curl_setopt($session, CURLOPT_HEADER, false);
+		// Tell PHP not to use SSLv3 (instead opting for TLS)
+		curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+		curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+		// obtain response
+		$response = curl_exec($session);
+		curl_close($session);
+		// print everything out
+		$reponse = json_decode($response);
+		echo $response->message;
+	}
 }
--- application/views/publik-templategejayan/register/isifile.php
+++ application/views/publik-templategejayan/register/isifile.php
@@ -9,78 +9,97 @@ if(!empty($script))echo '<script>$(document).ready(function(){'.$script.'});</sc
 					<div class="contact-form">
 						<h2><?php echo $title;?></h2>
 						<br/>
-						<form name="myForm" method="post" action="<?php echo site_url('dashboard/promoaction?act=editprocess')?>" enctype="multipart/form-data">
+						<form name="myForm" method="post" action="<?php echo site_url('register/process#error')?>" enctype="multipart/form-data">
 							<h3><strong>Data Pemilik</strong></h3>
 							<div>
 								<span><label>Nama Lengkap <br/>sesuai KTP/Tanda Pengenal</label></span>
-								<span><input name="promo[Judul]" type="text" class="textbox" value="" ng-model="title" value=""></span>
+								<span><input name="pemilik[nama]" type="text" class="textbox" value="<?php if(!empty($pemilik['nama']))echo $pemilik['nama'];?>"></span>
 							</div>
 							<div>
 								<span><label>Nomor Kartu Tanda Pengenal (KTP / tanda pengenal lainnya)</label></span>
-								<span><input name="promo[Judul]" type="text" class="textbox" value="" ng-model="title" value=""></span>
+								<span><input name="pemilik[noid]" type="text" class="textbox" value="<?php if(!empty($pemilik['noid']))echo $pemilik['noid'];?>"></span>
 							</div>
 							<div>
 								<span><label>No Telp / HP</label></span>
-								<span><input name="promo[Judul]" type="text" class="textbox" value="" ng-model="title" value=""></span>
+								<span><input name="pemilik[notelp]" type="text" class="textbox" value="<?php if(!empty($pemilik['notelp']))echo $pemilik['notelp'];?>"></span>
 							</div>
 							<div>
 								<span><label>Email <br/>data username dan password untuk login akan dikirimkan disini setelah proses verifikasi data selesai</label></span>
-								<span><input name="promo[Judul]" type="text" class="textbox" value="" ng-model="title" value=""></span>
+								<span><input name="pemilik[email]" type="text" class="textbox" value="<?php if(!empty($pemilik['email']))echo $pemilik['email'];?>" ></span>
 							</div>
 							<div>
 								<span><label>Domisili Sekarang</label></span>
-								<span><textarea name="promo[Judul]" type="text" class="textbox" value="" ng-model="title" value=""></textarea></span>
+								<span><textarea name="pemilik[domisili]" type="text" class="textbox"><?php if(!empty($pemilik['domisili']))echo $pemilik['domisili'];?></textarea></span>
 							</div>
 							<div>
 								<span><label>Upload Scan KTP / tanda pengenal lain<br/>maks 1 MB, support jpg, png</label></span>
-								<span><input type="file"></span>
+								<span><input name="idcard" type="file"></span>
 							</div>
 							<br/>
 							<h3><strong>Data Usaha</strong></h3>
 							<div>
 								<span><label>Nama Toko / Tempat Usaha</label></span>
-								<span><input name="promo[Judul]" type="text" class="textbox" value="" ng-model="title" value=""></span>
+								<span><input name="usaha[nama]" type="text" class="textbox" value="<?php if(!empty($usaha['nama']))echo $usaha['nama'];?>"></span>
 							</div>
 							<div>
-								<span><label>Deskrisi Singkat Toko / Tempat Usaha</label></span>
-								<span><textarea name="promo[Deskripsi]" type="text" class="textbox"></textarea></span>
+								<span><label>Kategori Usaha</label></span>
+								<span>
+								<select name="usaha[kategori]" required>
+								<option value="">Pilih kategori usaha</option>	
+								<?php foreach($this->db->get('kategoriUsaha')->result_array() as $ku):?>
+								<option value="<?php echo $ku['idkategoriUsaha'];?>"><?php echo $ku['namaKategoriUsaha'];?></option>	
+								<?php endforeach;?>
+								</select>
+							</div>
+							<div>
+								<span><label>Deskripsi Singkat Toko / Tempat Usaha</label></span>
+								<span><textarea name="usaha[deskripsi]" type="text" class="textbox"><?php if(!empty($usaha['deskripsi']))echo $usaha['deskripsi'];?></textarea></span>
 							</div>
 							<div>
 								<span><label>Alamat Toko</label></span>
-								<span><textarea name="promo[Deskripsi]" type="text" class="textbox">Jl. Gejayan No ? , Kabuaten Sleman, Daerah Istimewa Yogyakarta</textarea></span>
+								<span><textarea name="usaha[alamat]" type="text" class="textbox">Jl. Gejayan No ? , Kabuaten Sleman, Daerah Istimewa Yogyakarta</textarea></span>
 								
 							</div>
 							<div>
 								<span><label>Nomor Telepon</label></span>
-								<span><input name="promo[Judul]" type="text" class="textbox" value="" ng-model="title" value=""></span>
+								<span><input name="usaha[telepon]" type="text" class="textbox" value="<?php if(!empty($usaha['telepon']))echo $usaha['telepon'];?>" ></span>
 								
 							<div>
 								<span><label>Email (jika ada)</label></span>
-								<span><input name="promo[Judul]" type="text" class="textbox" value="" ng-model="title" value=""></span>
+								<span><input name="usaha[email]" type="text" class="textbox" value="<?php if(!empty($usaha['email']))echo $usaha['email'];?>"></span>
 								
 							</div>
 							<div>
 								<span><label>Jam Buka <br/>Contoh : 08:00 , penulisan 24 jam</label></span>
-								<span><input name="promo[Judul]" type="text" class="textbox" value="" ng-model="title" value=""></span>
+								<span><input name="usaha[jambuka]" type="text" class="textbox" value="<?php if(!empty($usaha['jambuka']))echo $usaha['jambuka'];?>"></span>
 								
 							</div>
 							<div>
 								<span><label>Jam Tutup <br/>Contoh : 21:00 , penulisan 24 jam</label></span>
-								<span><input name="promo[Judul]" type="text" class="textbox" value="" ng-model="title" value=""></span>
+								<span><input name="usaha[jamtutup]" type="text" class="textbox" value="<?php if(!empty($usaha['jamtutup']))echo $usaha['jamtutup'];?>"></span>
 								
 							</div>
 							<div>
 								<span><label>Hari Libur <br/>Contoh : minggu, senin , selasa. pisahkan dengan spasi</label></span>
-								<span><input name="promo[Judul]" type="text" class="textbox" value="" ng-model="title" value=""></span>								
+								<span><input name="usaha[libur]" type="text" class="textbox" value="<?php if(!empty($usaha['libur']))echo $usaha['libur'];?>"></span>								
 							</div>
 							<div>
 								<span><label>Logo Toko<br/>maks 1 MB, support jpg, png</label></span>
-								<span><input type="file"></span>
+								<span><input name="logotoko" type="file"></span>
 							</div>
 							<div>
-								<span><label>Scan Bukti Kepemilikan Toko, sialhkan upload salah satu(Surat Domisili Ssaha, Surat Ijin Usaha)<br/>maks 1 MB, support jpg, png</label></span>
-								<span><input type="file"></span>
+								<span><label>Scan Bukti 1<br/>maks 1 MB, support jpg, png</label></span>
+								<span><input name="bukti1" type="file"></span>
 							</div>
+							<div>
+								<span><label>Scan Bukti 2<br/>maks 1 MB, support jpg, png</label></span>
+								<span><input name="bukti2" type="file"></span>
+							</div>
+							<div>
+								<span><label>Scan Bukti 3<br/>maks 1 MB, support jpg, png</label></span>
+								<span><input name="bukti3" type="file"></span>
+							</div>
+							<?php if(!empty($error))echo $error;//if error found?>
 							<span><input type="submit" class="" value="Daftar"></span>
 							</div>
 						</form>		
